e60558ed97363d214f767506fd9c1aff63ec54b0
AnnaK1391 <anna_bel_13@mail.ru>
2022-12-12 19:38:19 +0300

    homework4-finish

--- app/src/main/java/com/example/homework_2/MainActivity.kt
+++ app/src/main/java/com/example/homework_2/MainActivity.kt
@@ -4,26 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    lateinit var firstScreen: FirstScreen
-    lateinit var secondScreen: SecondScreen
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        firstScreen = FirstScreen.newInstance()
-        secondScreen = SecondScreen.newInstance()
+
     }
 
-    fun tocatpage() {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(com.google.android.material.R.id.container, firstScreen)
-            .commit()
-    }
 
-    fun todogpage() {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(com.google.android.material.R.id.container, secondScreen)
-            .commit()
-        }
     }
--- app/src/main/java/com/example/homework_2/SecondScreen.kt
+++ app/src/main/java/com/example/homework_2/SecondScreen.kt
@@ -6,23 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 
 class SecondScreen : Fragment(R.layout.fragment_second_screen4) {
+    lateinit var controller: NavController
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        controller = findNavController()
         val button = view.findViewById<Button>(R.id.tocatpage)
         button.setOnClickListener {
-            (requireActivity() as MainActivity).tocatpage()
+            controller.navigate(R.id.action_secondScreen_to_firstScreen)
+
 
         }
     }
 
 
-    companion object {
-        @JvmStatic
-        fun newInstance() =
-            SecondScreen()
-    }
 
 }
